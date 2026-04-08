fae91adbf67207283bed07f5e5a3cb7fbc1ae1c9
juaristi22 <juaristi@uni.minerva.edu>
2026-04-08 20:03:54 +0530

    Update previewing url

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isVercelProduction = process.env.VERCEL_ENV === 'production';
+
 const nextConfig: NextConfig = {
   assetPrefix:
     process.env.NODE_ENV === 'production'
-      ? 'https://policyengine-model-phi.vercel.app'
+      ? isVercelProduction
+        ? 'https://policyengine-model-phi.vercel.app'
+        : `https://${process.env.VERCEL_URL}`
       : undefined,
   turbopack: {
     root: process.cwd(),
