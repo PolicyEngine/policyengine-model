2fbb4cbb097ad0b7e954bd327de8ef4e09629407
juaristi22 <juaristi@uni.minerva.edu>
2026-04-08 20:15:57 +0530

    New preview attempt

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from 'next';
 
-const isVercelProduction = process.env.VERCEL_ENV === 'production';
-
 const nextConfig: NextConfig = {
   assetPrefix:
-    process.env.NODE_ENV === 'production'
-      ? isVercelProduction
-        ? 'https://policyengine-model-phi.vercel.app'
-        : `https://${process.env.VERCEL_URL}`
+    process.env.VERCEL_ENV === 'production' && process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
       : undefined,
   turbopack: {
     root: process.cwd(),
